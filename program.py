08edf3a2510c7080bc795d37cd24b3bd99f98336
GaneshKamble5885 <kambleganesh0824@gmail.com>
2026-06-24 17:57:16 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,20 @@
+info={
+    "Name":"ganesh kamble",
+    "Age":21,
+    "City":"kolhapur",
+    "Addres": "murgud",
+    "Email":"gk9282213@gmail.com",
+    "Mob_no":8830485885
+}
+print('Student information: ')
+for key in info:
+    print(key, ":",info[key]," | type :",type(info[key]))
+
+
+
+
+
+
+
+
+
